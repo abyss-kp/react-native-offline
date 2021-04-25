6b2fbe5b964a97fb513b0201e824015b34788457
kapil-pandey <abyss6271@gmail.com>
2021-04-25 18:18:48 +0530

    Redux thunk and Persist setup

--- App.js
+++ App.js
@@ -10,51 +10,55 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StackNav } from './components/Stacks';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import { Provider } from 'react-redux';
-import { store } from './store';
+import { configurePersistentStore } from './store';
+import { PersistGate } from 'redux-persist/integration/react';
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
+const { store, persistor } = configurePersistentStore();
 export default function App() {
 	return (
 		<Provider store={store}>
-			<NavigationContainer style={{ paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0 }} ref={navigationRef}>
-				<Tab.Navigator
-					initialRouteName="Home"
-					headerMode="screen"
-					tabBarOptions={{
-						activeTintColor: '#000',
-						inactiveTintColor: '#EBEDF0',
-						activeBackgroundColor: '#fff',
-						inactiveBackgroundColor: '#000',
-						labelPosition: 'beside-icon'
-						// safeAreaInsets: { bottom: 5 }
-					}}
-				>
-					<Tab.Screen
-						name="Active"
-						component={StackNav('Active', HomePage)}
-						options={{
-							tabBarLabel: 'Active',
-							tabBarIcon: ({ color, size }) => <MaterialCommunityIcons name="notebook-outline" color={color} size={size} />
+			<PersistGate loading={null} persistor={persistor}>
+				<NavigationContainer style={{ paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0 }} ref={navigationRef}>
+					<Tab.Navigator
+						initialRouteName="Home"
+						headerMode="screen"
+						tabBarOptions={{
+							activeTintColor: '#000',
+							inactiveTintColor: '#EBEDF0',
+							activeBackgroundColor: '#fff',
+							inactiveBackgroundColor: '#000',
+							labelPosition: 'beside-icon'
+							// safeAreaInsets: { bottom: 5 }
 						}}
-					/>
-					<Tab.Screen
-						name="Completed"
-						component={StackNav('Completed', HomePage)}
-						options={{
-							tabBarLabel: 'Completed',
-							tabBarIcon: ({ color, size }) => <MaterialCommunityIcons name="check-all" color={color} size={size} />
-						}}
-					/>
-					<Tab.Screen
-						name="All"
-						component={StackNav('All', HomePage)}
-						options={{
-							tabBarLabel: 'All',
-							tabBarIcon: ({ color, size }) => <MaterialCommunityIcons name="history" color={color} size={size} />
-						}}
-					/>
-				</Tab.Navigator>
-			</NavigationContainer>
+					>
+						<Tab.Screen
+							name="Active"
+							component={StackNav('Active', HomePage)}
+							options={{
+								tabBarLabel: 'Active',
+								tabBarIcon: ({ color, size }) => <MaterialCommunityIcons name="notebook-outline" color={color} size={size} />
+							}}
+						/>
+						<Tab.Screen
+							name="Completed"
+							component={StackNav('Completed', HomePage)}
+							options={{
+								tabBarLabel: 'Completed',
+								tabBarIcon: ({ color, size }) => <MaterialCommunityIcons name="check-all" color={color} size={size} />
+							}}
+						/>
+						<Tab.Screen
+							name="All"
+							component={StackNav('All', HomePage)}
+							options={{
+								tabBarLabel: 'All',
+								tabBarIcon: ({ color, size }) => <MaterialCommunityIcons name="history" color={color} size={size} />
+							}}
+						/>
+					</Tab.Navigator>
+				</NavigationContainer>
+			</PersistGate>
 		</Provider>
 	);
 }
--- store.js
+++ store.js
@@ -1,4 +1,20 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers/rootReducer';
+import thunk from 'redux-thunk';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { persistStore, persistReducer } from 'redux-persist';
 
-export const store = createStore(rootReducer);
+const persistConfig = {
+	key: 'root',
+	storage: AsyncStorage,
+	whitelist: []
+};
+const initialState = {};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const configurePersistentStore = () => {
+	const store = createStore(persistedReducer, initialState, applyMiddleware(thunk));
+	const persistor = persistStore(store);
+
+	return { store, persistor };
+};
